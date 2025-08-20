738595d0abadf89498e9de27ac4d95e74db8568d
Michael Germini <michael@germini.info>
2025-08-20 02:21:26 +0200

    fix: Remove unused Suspense import from App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { Overview } from './pages/Overview';
